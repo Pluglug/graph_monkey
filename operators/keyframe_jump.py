2ac2e0b302e063a1f70594aab681990cdd00a3a5
F <hello@world.e.com>
2025-07-09 17:39:40 +0900

    キーフレームジャンプ機能を拡張し、範囲内でのジャンプを実装。タイムラインエリアの可視状態を考慮し、ループ機能を追加。

--- operators/keyframe_jump.py
+++ operators/keyframe_jump.py
@@ -1,5 +1,6 @@
 import bpy
-from bpy.types import Operator, BoolProperty
+from bpy.types import Operator, Context, Area
+from bpy.props import BoolProperty
 
 from ..utils.logging import get_logger
 
@@ -19,32 +20,92 @@ def jump_or_wrap(next=True):
             bpy.context.scene.frame_set(bpy.context.scene.frame_end)
 
 
-def jump_within_range(next=True):
-    scene = bpy.context.scene
-    current_frame = scene.frame_current
-    start_frame = scene.frame_start
-    end_frame = scene.frame_end
+class MONKEY_OT_JUMP_WITHIN_RANGE(Operator):
+    bl_idname = "keyframe.jump_within_range"
+    bl_label = "Jump within range"
+    bl_description = "Jump within range"
+    bl_options = set()
 
-    bpy.ops.screen.keyframe_jump(next=next)
-    new_frame = scene.frame_current
+    next: BoolProperty(default=True, options={"SKIP_SAVE"})
+    loop: BoolProperty(default=True, options={"SKIP_SAVE"})
 
-    loop = bpy.context.scene.keyframe_jump_wrap
+    def execute(self, context: Context):
+        scene = context.scene
 
-    if bpy.context.scene.frame_current == current_frame and loop:
-        # キーフレームが存在しない場合の処理
-        if next:
-            bpy.context.scene.frame_set(bpy.context.scene.frame_start)
+        if scene.use_preview_range:
+            self.start_frame = scene.frame_preview_start
+            self.end_frame = scene.frame_preview_end
         else:
-            bpy.context.scene.frame_set(bpy.context.scene.frame_end)  # 使い勝手が悪い
+            self.start_frame = scene.frame_start
+            self.end_frame = scene.frame_end
 
-    # ジャンプ後のフレームが再生範囲外の場合は範囲内に戻す
-    if new_frame < start_frame or new_frame > end_frame:
-        if next:
-            scene.frame_set(start_frame)
+        timeline_area = self.find_area_with_visible_fcurves(context)
+        if timeline_area:
+            with context.temp_override(area=timeline_area):
+                return self.jump_within_range(context)
         else:
-            scene.frame_set(end_frame)
+            # タイムラインエリアがない場合は通常ジャンプ
+            self.report({"INFO"}, "No Timeline area found")
+            bpy.ops.screen.keyframe_jump(next=self.next)
+            return {"FINISHED"}
+
+    @staticmethod
+    def find_area_with_visible_fcurves(context: Context) -> Area | None:
+        """
+        context.window.screen.areas から visible_fcurves を持つエリアを探して返す。
+        見つからなければ None。
+        """
+        for area in context.window.screen.areas:
+            with context.temp_override(area=area):
+                if hasattr(context, "visible_fcurves"):
+                    return area
+        return None
+
+    @staticmethod
+    def visible_key_on_current_frame(context: Context) -> bool:
+        """
+        contextとタイムラインエリアを受け取り、
+        現在のフレーム位置に可視状態のキーフレームがあればTrue、なければFalseを返す
+        """
+        scene = context.scene
+        current_frame = scene.frame_current
+
+        visible_fcurves = (
+            context.visible_fcurves if hasattr(context, "visible_fcurves") else []
+        )
+        for fcurve in visible_fcurves:
+            for kp in fcurve.keyframe_points:
+                if int(kp.co.x) == current_frame:
+                    return True
+        return False
+
+    def jump_within_range(self, context: Context):
+        scene = context.scene
+        current_frame = scene.frame_current
+
+        bpy.ops.screen.keyframe_jump(next=self.next)
+        new_frame = scene.frame_current
+
+        out_of_range = new_frame < self.start_frame or new_frame > self.end_frame
+        did_not_move = new_frame == current_frame
+
+        if (did_not_move or out_of_range) and self.loop:
+            # 逆端に移動
+            if self.next:
+                scene.frame_set(self.start_frame)
+            else:
+                scene.frame_set(self.end_frame)
+
+            # 逆端に可視キーフレームがあれば終了
+            if self.visible_key_on_current_frame(context):
+                return {"FINISHED"}
+
+            # なければもう一度ジャンプ
+            bpy.ops.screen.keyframe_jump(next=self.next)
+        return {"FINISHED"}
 
 
+# TODO: Prefsで定義する
 # def register():
 #     bpy.types.Scene.keyframe_jump_wrap = BoolProperty(
 #         name="Loop Keyframe Jump",
